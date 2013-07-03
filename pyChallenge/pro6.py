602ea91061bbb27c75eae4dddeec60402e0ba8d6
mohuanlanjing <mohuanlanjing@163.com>
2013-07-03 10:53:11 +0800

    * pro6 not collect comments

--- pyChallenge/pro6.py
+++ pyChallenge/pro6.py
@@ -3,5 +3,24 @@ http://www.pythonchallenge.com/pc/def/channel.html
 """
 
 import zipfile
+import re
 
-#print zip(a,b)
+z = zipfile.ZipFile('channel.zip','r')
+zlist = z.namelist()
+pattern = re.compile(r'\d+')
+start = '90052'
+s = zlist.index(start+'.txt')
+while True:
+    f = z.read(zlist[s])
+    try:
+        next = pattern.search(f).group()
+        print next
+    except:
+        print f
+        break
+    s = zlist.index(next+'.txt')
+
+
+
+
+
