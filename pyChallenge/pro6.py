12350c6dd62fbcf89f6c48736e93c8c5b98de2bf
mohuanlanjing <mohuanlanjing@163.com>
2013-07-04 11:36:24 +0800

    * pro6 finished

--- pyChallenge/pro6.py
+++ pyChallenge/pro6.py
@@ -8,19 +8,17 @@ import os
 
 z = ZipFile('channel.zip','r')
 zlist = z.namelist()
-#pattern = re.compile(r'\d+')
-#start = '90052'
-#s = zlist.index(start+'.txt')
-#while True:
-#    f = z.read(zlist[s])
-#    try:
-#        next = pattern.search(f).group()
-#        print next
-#    except:
-#        print f
-#        break
-#    s = zlist.index(next+'.txt')
+pattern = re.compile(r'\d+')
+start = '90052'
+s = zlist.index(start+'.txt')
+cmlist = [z.getinfo(start+'.txt').comment]
+while True:
+    f = z.read(zlist[s])
+    try:
+        next = pattern.search(f).group()
+    except:
+        break
+    s = zlist.index(next+'.txt')
+    cmlist.append(z.getinfo(next+'.txt').comment)
 
-cmlist = []
-
-print ''.join([z.getinfo(i).comment for i in zlist])
+print ''.join(cmlist)
