74de5643a01dd11e6c191e37d74e401652606507
mohuanlanjing <mohuanlanjing@163.com>
2013-07-04 18:29:25 +0800

    * pro7 not finish

--- pyChallenge/pro7.py
+++ pyChallenge/pro7.py
@@ -1,3 +1,14 @@
 """
 http://www.pythonchallenge.com/pc/def/oxygen.html
 """
+
+import Image
+import os
+
+img = Image.open('oxygen.png')
+#print os.linesep.join(dir(img))
+r,g,b,a = img.split()
+x,y = img.size
+for i in range(x):
+    for j in range(45, 55):
+        print img.getpixel(zip([i],[j])[0])
