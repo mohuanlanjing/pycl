cde14c801932752a25bd067124b92e345aea3667
mohuanlanjing <mohuanlanjing@163.com>
2013-07-04 11:06:34 +0800

    *pro6 close to answer

--- pyChallenge/pro6.py
+++ pyChallenge/pro6.py
@@ -2,25 +2,25 @@
 http://www.pythonchallenge.com/pc/def/channel.html
 """
 
-import zipfile
+from zipfile import ZipFile
 import re
+import os
 
-z = zipfile.ZipFile('channel.zip','r')
+z = ZipFile('channel.zip','r')
 zlist = z.namelist()
-pattern = re.compile(r'\d+')
-start = '90052'
-s = zlist.index(start+'.txt')
-while True:
-    f = z.read(zlist[s])
-    try:
-        next = pattern.search(f).group()
-        print next
-    except:
-        print f
-        break
-    s = zlist.index(next+'.txt')
+#pattern = re.compile(r'\d+')
+#start = '90052'
+#s = zlist.index(start+'.txt')
+#while True:
+#    f = z.read(zlist[s])
+#    try:
+#        next = pattern.search(f).group()
+#        print next
+#    except:
+#        print f
+#        break
+#    s = zlist.index(next+'.txt')
 
+cmlist = []
 
-
-
-
+print ''.join([z.getinfo(i).comment for i in zlist])
