db358f81b40e5881e89110b3b436d4bc388f4a08
mohuanlanjing <mohuanlanjing@163.com>
2013-07-05 10:03:06 +0800

    * pro7 finished

--- pyChallenge/pro7.py
+++ pyChallenge/pro7.py
@@ -3,12 +3,17 @@ http://www.pythonchallenge.com/pc/def/oxygen.html
 """
 
 import Image
-import os
 
 img = Image.open('oxygen.png')
-#print os.linesep.join(dir(img))
 r,g,b,a = img.split()
 x,y = img.size
+l = []
 for i in range(x):
-    for j in range(45, 55):
-        print img.getpixel(zip([i],[j])[0])
+    l.append(chr(r.getpixel(zip([i],[50])[0])))
+
+l = l[::7]
+print ''.join(l)
+
+print ''.join([chr(i) for i in [105, 110, 116, 101, 103, 114, 105, 116, 121]])
+
+
