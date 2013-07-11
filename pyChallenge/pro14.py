b4f8fc5cc16110d7f0f9f1a0a2fcdb24c0e58b94
mohuanlanjing <mohuanlanjing@163.com>
2013-07-11 17:36:57 +0800

    * pro 14 not finish

---
+++ pyChallenge/pro14.py
@@ -0,0 +1,24 @@
+"""
+http://www.pythonchallenge.com/pc/return/italy.html
+"""
+
+import Image
+im = Image.open('wire.png')
+w,h = im.size
+l1 = []
+for j in range(h):
+    for i in range(w):
+        l1.append(im.getpixel((i,j)))
+
+l2 = [(i,i-1,i-1,i-2) for i in range(100, 1, -2)]
+new = Image.new(im.mode, (100, 100))
+top = 1
+left = 1
+
+for walk in l2:
+    for i in walk:
+        count = 1
+        for j in range(i):
+            pass
+
+                
