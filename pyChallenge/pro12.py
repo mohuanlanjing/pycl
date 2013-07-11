69fc82ea143a0aa2b2ab7f1cc677676ba08e083e
mohuanlanjing <mohuanlanjing@163.com>
2013-07-11 11:49:00 +0800

    * pro12 don't konw what happen

--- pyChallenge/pro12.py
+++ pyChallenge/pro12.py
@@ -1,5 +1,15 @@
 """
 http://www.pythonchallenge.com/pc/return/evil.html
 """
-
+import Image
 f = file("evil2.gfx",'rb')
+content = f.read()
+f.close()
+
+for i in range(5):
+    ret = file(r'pro12/%d.png'%i, 'wb+')
+    ret.write(content[i::5])
+    Image.open('pro12/%d.png'%i).show()
+    ret.close()
+
+  
