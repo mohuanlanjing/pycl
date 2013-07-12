20890a6c2092fccf1b9f6cc8da557b38d6dd3e3c
mohuanlanjing <mohuanlanjing@163.com>
2013-07-12 16:45:56 +0800

    * pro 15 finish

--- pyChallenge/pro15.py
+++ pyChallenge/pro15.py
@@ -1,3 +1,9 @@
 """
 http://www.pythonchallenge.com/pc/return/cat.html
 """
+
+import calendar
+for i in range(1006, 1997, 10):
+    if calendar.isleap(i):
+        if calendar.weekday(i, 1, 1) == 3:
+            print i
