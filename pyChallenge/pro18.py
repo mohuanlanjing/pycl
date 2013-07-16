102b53a3eaac690d2a8d6975aea8dcbfb3956254
mohuanlanjing <mohuanlanjing@163.com>
2013-07-16 18:26:35 +0800

    * pro18 not finish:

--- pyChallenge/pro18.py
+++ pyChallenge/pro18.py
@@ -3,4 +3,13 @@
 http://www.pythonchallenge.com/pc/return/balloons.html
 """
 
+import difflib
+
 f = file('delta.txt')
+
+count = len(f.readline())
+half = count/2
+l2 = l1 = []
+print f.readlines()
+for i in f.readlines():
+    print i
